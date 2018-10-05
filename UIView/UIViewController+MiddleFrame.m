1a2d101c677d5d4b256355bffc1078a02f212566
coody <coody0829@gmail.com>
2018-10-05 17:52:17 +0800

    [.fix ][.CommonCategory ] 修正 UIViewcontroller+MiddleFrame 會造成閃退問題

--- UIView/UIViewController+MiddleFrame.m
+++ UIView/UIViewController+MiddleFrame.m
@@ -14,7 +14,7 @@
 @implementation UIViewController (MiddleFrame)
 
 -(CGRect)getMiddleFrame{
-    CGRect tempFrame = [[self class] getSafeAreaFrame];
+    CGRect tempFrame = [self getSafeAreaFrame];
     CGFloat statusBarHeight = ( ([SafeAreaTool getSafeAreaInsets].top > 0) ? 0.0f : [[UIApplication sharedApplication] statusBarFrame].size.height);
     tempFrame = CGRectMake(tempFrame.origin.x,
                            tempFrame.origin.y +
@@ -25,12 +25,12 @@
                            statusBarHeight -
                            self.navigationController.navigationBar.frame.size.height -
                            self.tabBarController.tabBar.frame.size.height +
-                           [[self class] getSafeAreaInsets].bottom);
+                           [self getSafeAreaInsets].bottom);
     return tempFrame;
 }
 
 #pragma mark - Private
-+(UIEdgeInsets)getSafeAreaInsets{
+-(UIEdgeInsets)getSafeAreaInsets{
     UIEdgeInsets safeInsets;
     if ( @available( iOS 11.0 , * ) ) {
         safeInsets = [[[UIApplication sharedApplication] delegate] window].safeAreaInsets;
@@ -38,6 +38,16 @@
         safeInsets = [[[UIApplication sharedApplication] delegate] window].alignmentRectInsets;
     }
     return safeInsets;
+}
+
+-(CGRect)getSafeAreaFrame{
+    CGRect safeFrame;
+    if ( @available( iOS 11.0 , * ) ) {
+        safeFrame = UIEdgeInsetsInsetRect([UIScreen mainScreen].bounds, [[[UIApplication sharedApplication] delegate] window].safeAreaInsets);
+    } else {
+        safeFrame = [UIScreen mainScreen].bounds;
+    }
+    return safeFrame;
 }
 
 @end
